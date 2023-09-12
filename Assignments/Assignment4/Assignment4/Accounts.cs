800f281d48616f0273ed3dc2b788461b3ec38d88
Chandankumar Renuka <chandankumarrr@infinite.com>
2023-09-12 20:04:57 +0530

    Assignment 4

---
+++ Assignments/Assignment4/Assignment4/Accounts.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment4
+{
+    public class Accounts
+    {
+        public int accountNo;
+        public string customerName;
+        public string accountType;
+        public string transactionType;
+        public int amount;
+        public int balance;
+
+        public Accounts(int accountNo, string customerName, string accountType, string transactionType, int amount, int balance)
+        {
+            this.accountNo = accountNo; 
+            this.customerName = customerName;
+            this.accountType = accountType;
+            this.transactionType = transactionType;
+            this.amount = amount;
+            this.balance = balance;
+        }
+
+        public void UpdateBalance()
+        {
+            if (this.transactionType == "d")
+            {
+                this.balance += this.amount;
+                Console.WriteLine("Amount is deposited successfully");
+            }
+            else if (this.transactionType == "w")
+            {
+                if (this.balance < this.amount)
+                {
+                    Console.WriteLine("Insufficient balance");
+                }
+                else
+                {
+                    this.balance -= this.amount;
+                    Console.WriteLine("withdrawn successfully");
+                }
+            }
+        }
+
+        public void Credit(int amount)
+        {
+            this.amount = amount;
+            UpdateBalance();
+        }
+
+        public void Debit(int amount)
+        {
+            this.amount = amount;
+            UpdateBalance();
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("Account number: {0}", this.accountNo);
+            Console.WriteLine("Customer name: {0}", this.customerName);
+            Console.WriteLine("Account type: {0}", this.accountType);
+            Console.WriteLine("Transaction type: {0}", this.transactionType);
+            Console.WriteLine("Amount: {0}", this.amount);
+            Console.WriteLine("Balance: {0}", this.balance);
+        }
+
+        
+    }
+}
---
+++ Assignments/Assignment4/Assignment4/Program.cs
@@ -0,0 +1,124 @@
+﻿
+// 2) 2. Create a class called student which has data members like rollno, name, class, Semester, branch, int [] marks=new int marks [5](marks of 5 subjects )
+
+//-Pass the details of student like rollno, name, class, SEM, branch in constructor
+
+//- For marks write a method called GetMarks() and give marks for all 5 subjects
+
+//-Write a method called displayresult, which should calculate the average marks
+
+//-If marks of any one subject is less than 35 print result as failed
+//-If marks of all subject is >35, but average is < 50 then also print result as failed
+//-If avg > 50 then print result as passed.
+
+//-Write a DisplayData() method to display all values.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Student
+{
+    public int rollno;
+    public string name;
+    public string classname;
+    public string semester;
+    public string branch;
+    public int[] marks = new int[5];
+
+    public Student(int rollno, string name, string classname, string semester, string branch)
+    {
+        this.rollno = rollno;
+        this.name = name;
+        this.classname = classname;
+        this.semester = semester;
+        this.branch = branch;
+    }
+
+    public void GetMarks()
+    {
+        for (int i = 0; i < marks.Length; i++)
+        {
+            Console.WriteLine("Enter marks for subject {0}: ", i + 1);
+            marks[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    public void DisplayResult()
+    {
+        int totalMarks = 0;
+        foreach (int mark in marks)
+        {
+            totalMarks += mark;
+        }
+        double avgMarks = totalMarks / marks.Length;
+
+        string result;
+        if (marks[0] < 35)
+        {
+            result = "Failed";
+        }
+        else if (avgMarks < 50)
+        {
+            result = "Failed";
+        }
+        else
+        {
+            result = "Passed";
+        }
+
+        Console.WriteLine("Student Result:");
+        Console.WriteLine("Roll No: {0}", rollno);
+        Console.WriteLine("Name: {0}", name);
+        Console.WriteLine("Class: {0}", classname);
+        Console.WriteLine("Semester: {0}", semester);
+        Console.WriteLine("Branch: {0}", branch);
+        Console.WriteLine("Average Marks: {0}", avgMarks);
+        Console.WriteLine("Result: {0}", result);
+    }
+
+    public void DisplayData()
+    {
+        Console.WriteLine("Student Details:");
+        Console.WriteLine("Roll No: {0}", rollno);
+        Console.WriteLine("Name: {0}", name);
+        Console.WriteLine("Class: {0}", classname);
+        Console.WriteLine("Semester: {0}", semester);
+        Console.WriteLine("Branch: {0}", branch);
+    }
+    
+        public static void Main()
+        {
+            Console.WriteLine("-----1st question output-----");
+            // Create a new Student object
+            Student student = new Student(103026, "chandan", "degree", "8", "CSE");
+
+            // Get the marks for the student
+            student.GetMarks();
+
+            // Display the result of the student
+            student.DisplayResult();
+            
+            // Display the details of the student.
+            student.DisplayData();
+
+           
+            Console.WriteLine("--------------2nd question output-------------------");
+
+            Accounts account = new Accounts(103367, "chandan", "Savings", "d", 1000, 0);
+
+            // Deposit Rs. 500
+            account.Credit(500);
+
+            // Withdraw Rs. 200
+            account.Debit(200);
+
+            // Show the account details
+            account.ShowData();
+            Console.ReadLine();
+        }
+    }
+}
