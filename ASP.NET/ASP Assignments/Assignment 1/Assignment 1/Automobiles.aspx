eca01b38f5b780bf919cbfe1b7ce57beda90130a
Chandankumar Renuka <chandankumarrr@infinite.com>
2023-10-31 18:55:02 +0530

    ASP assignment 1

---
+++ ASP.NET/ASP Assignments/Assignment 1/Assignment 1/Automobiles.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Automobiles.aspx.cs" Inherits="Assignment_1.Automobiles" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="dropdown" runat="server" AutoPostBack="true" OnSelectedIndexChanged="dropdown_SelectedIndexChanged">
+                <asp:ListItem Text="Select Available Mobiles"/>
+                <asp:ListItem Text="Oppo A83" Value="1"  />
+                <asp:ListItem Text="Iphone 13" Value="2"  />
+                <asp:ListItem Text="Samsung" Value="3"  />
+            </asp:DropDownList>
+            <br />
+            <br />
+
+            <asp:Image ID="image"  runat="server" ImageUrl="~/images/normal.png" Width="300" Height="300" />
+
+            <br />
+            <br />
+
+            <asp:Button ID="btnShowCost" runat="server" Text="Show Cost" OnClick="btnShowCost_Click" />
+
+            <br />
+            <br />
+
+            <asp:Label ID="lableCost" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/ASP Assignments/Assignment 1/Assignment 1/Automobiles.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_1
+{
+    public partial class Automobiles : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+          
+        }
+
+        protected void btnShowCost_Click(object sender, EventArgs e)
+        {
+
+            string SelectedItem = dropdown.SelectedValue;
+            string cost = string.Empty;
+            if (SelectedItem == "1")
+            {
+
+                cost = "13000";
+
+            }
+
+            else if (SelectedItem == "2")
+            {
+
+                cost = "60000";
+            }
+
+            else if (SelectedItem == "3")
+            {
+
+                cost = "45000";
+            }
+
+            string Item = dropdown.SelectedItem.Text;
+
+            lableCost.Text = $"Cost of {Item}: {cost}";
+
+        }
+
+        protected void dropdown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            string selectedItem = dropdown.SelectedValue;
+           
+            if (selectedItem == "1")
+            {
+                image.ImageUrl = $"images/oppo.png";
+            }
+
+            else if (selectedItem == "2")
+            {
+                image.ImageUrl = $"Images/Iphone 13.png";
+
+            }
+
+            else if (selectedItem == "3")
+            {
+                image.ImageUrl = $"Images/Samsung.png";
+
+            }
+            else
+            {
+
+                image.ImageUrl = $"Images/{"Images/normal.jpg"}";
+            }
+        }
+    }
+}
